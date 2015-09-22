d6645274220deeb6af7ad4b6d585c83ba6ee3fc0
Nick <Nick@BatComputer>
2015-09-21 21:29:30 -0500

    IntroJDBC bookSample MySqlDb updated delete and create and update with prepare statement working

--- src/bookSample/MySqlDb.java
+++ src/bookSample/MySqlDb.java
@@ -83,7 +83,28 @@ public class MySqlDb{
         
     }
     
-    public void createOneRecord(String tableName, List newRecordCols, List newRecordValues) throws SQLException {
+    public void createRecordPrepareStatement(String tableName, List newRecordCols, List newRecordValues) throws SQLException {
+        
+        Object[] recordColsArray = newRecordCols.toArray();
+        Object[] recordValueArray = newRecordValues.toArray();
+        String recordColsString = recordColsArray[0].toString();
+        String recordValueString = "?";
+        for(int i = 1; i < recordColsArray.length; i++){
+            recordColsString += ", " + recordColsArray[i].toString(); 
+            recordValueString += ", ?"; 
+                    
+        }
+        
+        
+        String sql = "INSERT INTO " + tableName + " ( " + recordColsString + " ) VALUES (" + recordValueString + ")";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        
+        for(int i = 0; i < recordColsArray.length; i++){
+            stmt.setObject(i+1,recordValueArray[i]);
+        }
+        stmt.executeUpdate();
+    }
+    public void createRecord(String tableName, List newRecordCols, List newRecordValues) throws SQLException {
         
         Object[] recordColsArray = newRecordCols.toArray();
         Object[] recordValueArray = newRecordValues.toArray();
@@ -101,13 +122,12 @@ public class MySqlDb{
         stmt.executeUpdate(sql);
     }
     
-    public void updateByPrimaryKey(String tableName, String primaryKey, Object primaryKeyValue, Map<String,Object> updateRecord) throws SQLException {
+    public void updateByPrimaryKey(String tableName, String primaryKey, Object primaryKeyValue, List updateRecordCols, List updateRecordValues) throws SQLException {
 
-        Map<String, Object> record = updateRecord;
-        Object[] recordKeyArray = record.keySet().toArray();
-        Object[] recordObjectArray = record.values().toArray();
+        Object[] recordKeyArray = updateRecordCols.toArray();
+        Object[] recordObjectArray = updateRecordValues.toArray();
         String recordString = recordKeyArray[0].toString() + "= '" + recordObjectArray[0].toString() + "'";
-        for(int i = 1; i < recordKeyArray.length-1; i++){
+        for(int i = 1; i < recordKeyArray.length; i++){
             recordString += "," + recordKeyArray[i].toString() + "= '" + recordObjectArray[i].toString() + "'";
         }
         
@@ -119,6 +139,26 @@ public class MySqlDb{
         }
         Statement stmt = conn.createStatement();
         stmt.executeUpdate(sql);
+    }
+    public void updateByPrimaryKeyPrepareStatement(String tableName, String primaryKey, Object primaryKeyValue, List updateRecordCols, List updateRecordValues) throws SQLException {
+
+        Object[] recordKeyArray = updateRecordCols.toArray();
+        Object[] recordObjectArray = updateRecordValues.toArray();
+        String recordString = recordKeyArray[0].toString() + "= ?";
+        for(int i = 1; i < recordKeyArray.length; i++){
+            recordString += "," + recordKeyArray[i].toString() + "= ?";
+        }
+        
+        String sql = "UPDATE " + tableName + " SET "  + recordString + " WHERE " + primaryKey + " = ?";
+       
+        
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        for(int i = 0; i < recordKeyArray.length; i++){
+            stmt.setObject(i+1, recordObjectArray[i]);
+        }
+        stmt.setObject(recordKeyArray.length+1, primaryKeyValue);
+        stmt.executeUpdate();
+        
     }
     
     public static void main(String[] args) throws Exception {
@@ -147,26 +187,31 @@ public class MySqlDb{
         newRecordCols.add("author_name");
         newRecordCols.add("date_created");
         List newRecordValues = new ArrayList();      
-        newRecordValues.add("Jared Kowalske");
-        newRecordValues.add("1991-08-27");
-        db.createOneRecord("author", newRecordCols, newRecordValues);
+        newRecordValues.add("Pekka Rinne");
+        newRecordValues.add("1989-04-02");
+        db.createRecordPrepareStatement("author", newRecordCols, newRecordValues);
         
         records = db.findAllRecords("author");
         for(Map record:records){
             System.out.println(record);
         }
-        
         */
+        
         System.out.println("Create = Working");
         System.out.println("U - Update:");
-//        Map<String,Object> updateRecord = new HashMap();
-//        updateRecord.put("author_name", "Bill Smith");
-//        db.updateByPrimaryKey("author", "author_id", "3",updateRecord);
-//        
-//        records = db.findAllRecords("author");
-//        for(Map record:records){
-//            System.out.println(record);
-//        }
+        
+        List updateRecordCols = new ArrayList();
+        updateRecordCols.add("author_name");
+        updateRecordCols.add("date_created");
+        List updateRecordValues = new ArrayList();      
+        updateRecordValues.add("Bill Murray");
+        updateRecordValues.add("1959-07-29");
+        db.updateByPrimaryKeyPrepareStatement("author", "author_id", "4", updateRecordCols, updateRecordValues);
+        
+        records = db.findAllRecords("author");
+        for(Map record:records){
+            System.out.println(record);
+        }
         
         System.out.println("Update = Working");
         
